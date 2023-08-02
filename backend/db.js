b819425b3a32af0f9422345184658de3c757af09
IbrahimGazi2022 <coder.ibrahimgazi@gmail.com>
2023-08-03 02:46:04 +0600

    Second Push

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://127.0.0.1:27017/ibrahim";
 
-const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {
-    console.log("Connect to mongo succesfully");
-  });
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Database is Connected");
+  } catch (error) {
+    console.log("Database is not connected");
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectToMongo;
